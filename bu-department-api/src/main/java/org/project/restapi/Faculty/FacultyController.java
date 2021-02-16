80fdd5848d9da4cb8fcc87dd83590d076d4a437b
sandeepam5588 <78194280+sandeepam5588@users.noreply.github.com>
2021-02-16 21:46:54 +0530

    day 1 completed

--- bu-department-api/src/main/java/org/project/restapi/Faculty/FacultyController.java
+++ bu-department-api/src/main/java/org/project/restapi/Faculty/FacultyController.java
@@ -4,38 +4,43 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/bu")
 public class FacultyController {
 	
 	@Autowired
 	private FacultyService facultyService;
 	
-	@RequestMapping("/api/v1/bu/faculty")
+	@RequestMapping("/faculty")
 	public List<Faculty> getAllFaculty() {
 		return facultyService.getAllFaculty();
 	}
 	
-	@RequestMapping("/api/v1/bu/faculty/{id}")
+	@RequestMapping("/faculty/{id}")
 	public Optional<Faculty> getFaculty(@PathVariable String id) {
 		return facultyService.getFaculty(id);
 	}
 	
-	@RequestMapping(method=RequestMethod.POST, value= "/api/v1/bu/faculty")
+	@RequestMapping(method=RequestMethod.POST, value= "/faculty")
 	public String addFaculty(@RequestBody Faculty faculty) {
 		return facultyService.addFaculty(faculty);
 	}
-	@RequestMapping(method = RequestMethod.PUT, value="/api/v1/bu/faculty/{id}")
+	@PutMapping("/faculty/{id}")
+	//@RequestMapping(method = RequestMethod.PUT, value="/faculty/{id}")
 	public String updateFaculty(@RequestBody Faculty faculty, @PathVariable String id) {
 		return facultyService.updateFaculty(faculty, id);
 	}
 	
-	@RequestMapping(method = RequestMethod.DELETE, value="/api/v1/bu/faculty/{id}")
+	@DeleteMapping("/faculty/{id}")
+	//@RequestMapping(method = RequestMethod.DELETE, value="/faculty/{id}")
 	public String deleteFaculty(@PathVariable String id) {
 		return facultyService.deleteFaculty(id);
 	}
--- bu-department-api/src/main/java/org/project/restapi/hello/HelloController.java
+++ bu-department-api/src/main/java/org/project/restapi/hello/HelloController.java
@@ -4,7 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController {  
+	
 	@RequestMapping("/api/v1/bu/hello")
 	public String sayHi() {
 		return "Hi..!";
